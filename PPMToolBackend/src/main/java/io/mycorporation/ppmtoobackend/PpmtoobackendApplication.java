e56157b72ad821cb41cbacff786c3c09940b7b20
milad <milad.yeganeh@gmail.com>
2019-05-10 15:48:31 +0430

    First Commit

---
+++ PPMToolBackend/src/main/java/io/mycorporation/ppmtoobackend/PpmtoobackendApplication.java
@@ -0,0 +1,13 @@
+package io.mycorporation.ppmtoobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoobackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoobackendApplication.class, args);
+    }
+
+}
---
+++ PPMToolBackend/src/test/java/io/mycorporation/ppmtoobackend/PpmtoobackendApplicationTests.java
@@ -0,0 +1,16 @@
+package io.mycorporation.ppmtoobackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmtoobackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
